aa82351f7d170051cb72f3ec7012560fd2e0fdcd
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-10-01 19:16:35 +0530

    Delete 01-10-24 directory

--- 01-10-24/task1/getTwodigitSum.java
+++
@@ -1,8 +0,0 @@
-package com.task7;
-
-public class getTwodigitSum{
-  public static void main(String[] args) {
-int res=	Sumoftwodigit.getTwodigitSum(12);
-System.out.println(res);
-}
-}
--- 01-10-24/task3/getNextMultipleOfHundred.java
+++
@@ -1,8 +0,0 @@
-package com.task9;
-
-public class getNextMultipleOfHundred {
-     public static void main(String [] args) {
-    	 int result = NextMultipleOfHundred.getNextMultipleOfHundred(302);
-    	 System.out.println(result);
-     }
-}
