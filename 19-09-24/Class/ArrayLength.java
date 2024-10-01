d687017657724d62bba737817b704e309372f39d
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-10-01 19:17:06 +0530

    Delete 19-09-24 directory

--- 19-09-24/Class/ArrayLength.java
+++
@@ -1,7 +0,0 @@
-public class ArrayLength{
-	public static void main(String[] args) 
-	{
-		
-		System.out.println("Array Length is"+args.length);
-	}
-}
--- 19-09-24/Class/GetSquare.java
+++
@@ -1,8 +0,0 @@
-public class GetSquare
-{
-	public static void main(String[] args) 
-	{
-		int num =Integer.parseInt(args[0]);
-		System.out.println("Square of "+num+" is : "+(num*num));
-	}
-}
