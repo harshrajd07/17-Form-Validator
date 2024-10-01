472cd15e2a70bb81846f0cadc97d907a9ffac8fe
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-10-01 19:16:22 +0530

    Delete 18-09-24 directory

--- 18-09-24/Addition.java
+++
@@ -1,8 +0,0 @@
-public class Addition{
-public static void main(String [] args){
-int a=10;
-int b=20;
-int addition=a+b;
-System.out.println("Addition of Two Numbers of: "+addition);
-}
-}
--- 18-09-24/area.java
+++
@@ -1,8 +0,0 @@
-public class area{
-public static void main(String [] args){
-int r=10;
-double pi=3.14;
-double area= pi*r*r;
-System.out.println("Area of Circle is: "+area);
-}
-}
