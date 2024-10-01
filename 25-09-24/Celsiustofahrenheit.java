27908cd4bc4eee3b23cd88f4a0b8f9bd08e65565
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-10-01 19:17:33 +0530

    Delete 25-09-24 directory

--- 25-09-24/Celsiustofahrenheit.java
+++
@@ -1,9 +0,0 @@
-public class Celsiustofahrenheit
-{
-	public static void main(String[] args) 
-	{
-		double celsius = Double.parseDouble(args[0]);
-		double fahrenheit = ((9*celsius)/5)+32;
-		System.out.println("fahrenheit: "+fahrenheit);
-	}
-}
